1491ea5ccb45b5551948ba43ccb40d9d38ce30dc
warriorstar-orion <orion@snowfrost.garden>
2025-06-05 11:12:23 -0400

    v0.1.12

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,9 +7,9 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'avulto'
-copyright = '2024 Warriorstar Orion'
+copyright = '2025 Warriorstar Orion'
 author = 'Warriorstar Orion'
-release = 'v0.1.11'
+release = 'v0.1.12'
 version = release
 
 extensions = [
